3f6519cd7c499f07862cea03d5d8e4ea7ca04e82
jaywomack <womackjw@hotmail.com>
2021-06-18 18:58:26 -0700

    working through conditionals

--- index.php
+++ index.php
@@ -1,35 +1,38 @@
 <?php
 
-// Normal Arrays
-$names = [
-  'Jay',
-  'Whit',
-  'Isis',
-  'Keera',
-  'Kaleb',
-  'Lincoln',
-  'William',
-  'Atlas'
+// // Normal Arrays
+// $names = [
+//   'Jay',
+//   'Whit',
+//   'Isis',
+//   'Keera',
+//   'Kaleb',
+//   'Lincoln',
+//   'William',
+//   'Atlas'
 
+// ];
+
+// // Normal Array
+// $animals = ['Kaleb','Lincoln','William','Atlas'];
+
+// // Associative Array
+// $person = [
+//   'age'=> 36,
+//   'name'=> 'Jay',
+//   'hair'=> 'Blonde',
+//   'Job'=> 'Web Developer'
+// ];
+
+// $person['name'] = "New Jay";
+
+// unset($person['age']);
+
+$task = [
+  'title' => 'Finish homework',
+  'due' => 'today',
+  'assigned_to' => 'Jeffrey',
+  'completed' => false,
 ];
-
-// Normal Array
-$animals = ['Kaleb','Lincoln','William','Atlas'];
-
-
-// Associative Array
-$person = [
-  'age'=> 36,
-  'name'=> 'Jay',
-  'hair'=> 'Blonde',
-  'Job'=> 'Web Developer'
-];
-
-
-$person['name'] = "New Jay";
-
-unset($person['age']);
-
-
 
 require 'index.view.php';
--- index.view.php
+++ index.view.php
@@ -17,12 +17,33 @@
   </style>
 </head>
 <body>
-  <header>
-    <?php foreach($person as $key => $feature):?>
-      <li><strong><?=$key;?></strong> <?= $feature;?></li>
-    <?php endforeach; ?>
-</header>
+  <h1>Task for the day</h1>
 
+  <ul>
+      <?php foreach ($task1 as $heading => $value): ?>
+        <li>
+          <strong><?= ucfirst($heading) ?>:  <?= $value ?></strong>
+        </li>
+      <?php endforeach; ?>
+  </ul>
+
+        <h2>Task inputted manually</h2>
+  <ul>
+        <li>
+          <strong>Name: <?= $task1['title'] ?></strong>
+        </li>
+        <li>
+          <strong>Due Date: <?= $task1['due'] ?></strong>
+        </li>
+        <li>
+          <strong>Assigned To: <?= $task1['assigned_to'] ?></strong>
+        </li>
+        <li>
+          <strong>Completed: <?= $task1['completed']
+            ? 'Complete'
+            : 'Incomplete' ?></strong>
+        </li>
+  </ul>
 
 
 </body>
