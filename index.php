b72e678cbc6c55244fa2b627197523b55fccbed3
jaywomack <womackjw@hotmail.com>
2021-06-19 20:54:24 -0700

    Using PDO to connect to database. Connection established.

--- index.php
+++ index.php
@@ -1,37 +1,5 @@
 <?php
-
-// // Normal Arrays
-$names = [
-  'Jay',
-  'Whit',
-  'Isis',
-  'Keera',
-  'Kaleb',
-  'Lincoln',
-  'William',
-  'Atlas',
-];
-
-// // Normal Array
-// $animals = ['Kaleb','Lincoln','William','Atlas'];
-
-// // Associative Array
-$person = [
-  'age' => 36,
-  'name' => 'Jay',
-  'hair' => 'Blonde',
-  'Job' => 'Web Developer',
-];
-
-// $person['name'] = "New Jay";
-
-// unset($person['age']);
-
-$task = [
-  'title' => 'Finish homework',
-  'due' => 'today',
-  'assigned_to' => 'Jeffrey',
-  'completed' => true,
-];
-
+require 'connect.php';
 require 'index.view.php';
+
+connect_db();
--- index.view.php
+++ index.view.php
@@ -17,30 +17,6 @@
   </style>
 </head>
 <body>
-<!-- List out the names of the names array -->
-    <h2>Family members:</h2>
-    <ul>
-      <?php foreach ($names as $name) {
-        echo "<li>$name</li>";
-      } ?>
-    </ul>
-
-<!-- Create a list of the keys and values in the person associative array -->
-      <ul>
-      <?php foreach ($person as $x => $x_value) {
-        echo "<li><strong>$x: </strong>$x_value</li>";
-      } ?>
-      </ul>
-<!-- Create a list of the keys and values in the task associative array -->
-      <ul>
-        <?php foreach ($task as $t => $t_value) {
-          if ($t == 'completed') {
-            echo "<li><strong>$t: </strong>&#10003;</li>";
-          } else {
-            echo "<li><strong>$t: </strong>$t_value</li>";
-          }
-        } ?>
-      </ul>
 
 </body>
 </html>
